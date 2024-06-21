5717a97905fbc2965051fc9a9d76b9dffce8d7f2
Alon Shaul <alon.shaul95@gmail.com>
2024-06-21 03:22:49 +0300

    v2

--- front/src/context/ApiContext.js
+++ front/src/context/ApiContext.js
@@ -4,8 +4,7 @@ const ApiContext = createContext();
 export const useApi = () => useContext(ApiContext);
 
 export const ApiProvider = ({ children }) => {
-  const renderURL = process.env.RENDER ;
-  const apiUrl = renderURL ? renderURL : 'http://localhost:8800'; 
+  const apiUrl = 'https://movementor-fyyf.onrender.com' ? 'https://movementor-fyyf.onrender.com' : 'http://localhost:8800'; 
 
   return (
     <ApiContext.Provider value={apiUrl}>
