f4ad51773e22158632ef594895c0cf008f1622b9
Alon Shaul <alon.shaul95@gmail.com>
2024-07-14 20:23:21 +0300

    Update Dashboard Charts

--- front/src/pages/Dashboard.jsx
+++ front/src/pages/Dashboard.jsx
@@ -1,15 +1,20 @@
-// front/src/pages/Dashboard.jsx
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useApi } from '../context/ApiContext';
-import { Bar } from 'react-chartjs-2';
-import 'chart.js/auto'; // Import this to automatically register Chart.js components
+import { Bar, Pie } from 'react-chartjs-2';
+import Chart from 'chart.js/auto';
+import ChartDataLabels from 'chartjs-plugin-datalabels'; // Import the plugin
+
+Chart.register(ChartDataLabels); // Register the plugin
 
 const Dashboard = () => {
   const [categoryRatings, setCategoryRatings] = useState({});
   const [likesDislikes, setLikesDislikes] = useState({});
   const [userRatingsCount, setUserRatingsCount] = useState([]);
-  const [postRatingsCount, setPostRatingsCount] = useState([]); // Add state for post ratings count
+  const [postRatingsCount, setPostRatingsCount] = useState([]);
+  const [exerciseRatings, setExerciseRatings] = useState([]);
+  const [topExercises, setTopExercises] = useState([]);
+  const [topDislikedExercises, setTopDislikedExercises] = useState([]); // State for top disliked exercises
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const apiUrl = useApi();
@@ -20,12 +25,18 @@ const Dashboard = () => {
         const ratingsResponse = await axios.get(`${apiUrl}/api/posts/category/ratings`);
         const likesDislikesResponse = await axios.get(`${apiUrl}/api/posts/category/likesdislikes`);
         const userRatingsCountResponse = await axios.get(`${apiUrl}/api/posts/user/ratingscount`);
-        const postRatingsCountResponse = await axios.get(`${apiUrl}/api/posts/ratings/count`); // Fetch post ratings count
+        const postRatingsCountResponse = await axios.get(`${apiUrl}/api/posts/ratings/count`);
+        const exerciseRatingsResponse = await axios.get(`${apiUrl}/api/posts/exercises/ratings`);
+        const topExercisesResponse = await axios.get(`${apiUrl}/api/posts/top/likes`);
+        const topDislikedExercisesResponse = await axios.get(`${apiUrl}/api/posts/top/dislikes`); // Fetch top disliked exercises
 
         setCategoryRatings(ratingsResponse.data);
         setLikesDislikes(likesDislikesResponse.data);
         setUserRatingsCount(userRatingsCountResponse.data);
-        setPostRatingsCount(postRatingsCountResponse.data); // Set post ratings count data
+        setPostRatingsCount(postRatingsCountResponse.data);
+        setExerciseRatings(exerciseRatingsResponse.data);
+        setTopExercises(topExercisesResponse.data);
+        setTopDislikedExercises(topDislikedExercisesResponse.data); // Set top disliked exercises data
         setLoading(false);
       } catch (err) {
         setError(err.message);
@@ -45,6 +56,10 @@ const Dashboard = () => {
     'rgba(75, 192, 192, 0.2)',
     'rgba(153, 102, 255, 0.2)',
     'rgba(255, 159, 64, 0.2)',
+    'rgba(50, 205, 50, 0.2)',
+    'rgba(255, 69, 0, 0.2)',
+    'rgba(0, 191, 255, 0.2)',
+    'rgba(255, 20, 147, 0.2)'
   ];
 
   const borderColor = [
@@ -54,6 +69,10 @@ const Dashboard = () => {
     'rgba(75, 192, 192, 1)',
     'rgba(153, 102, 255, 1)',
     'rgba(255, 159, 64, 1)',
+    'rgba(50, 205, 50, 1)',
+    'rgba(255, 69, 0, 1)',
+    'rgba(0, 191, 255, 1)',
+    'rgba(255, 20, 147, 1)'
   ];
 
   const ratingsData = {
@@ -107,7 +126,7 @@ const Dashboard = () => {
     datasets: [
       {
         label: 'Number of Ratings',
-        data: postRatingsCount.map(post => Math.round(post.ratingCount)), // Ensure integer values
+        data: postRatingsCount.map(post => Math.round(post.ratingCount)),
         backgroundColor: 'rgba(255, 159, 64, 0.2)',
         borderColor: 'rgba(255, 159, 64, 1)',
         borderWidth: 1,
@@ -115,14 +134,78 @@ const Dashboard = () => {
     ],
   };
 
+  const exerciseRatingsData = {
+    labels: exerciseRatings.map(exercise => `${exercise.title} (${exercise.category})`),
+    datasets: [
+      {
+        label: 'Average Rating',
+        data: exerciseRatings.map(exercise => parseFloat(exercise.averageRating)),
+        backgroundColor: colors.slice(0, exerciseRatings.length),
+        borderColor: borderColor.slice(0, exerciseRatings.length),
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const topExercisesData = {
+    labels: topExercises.map(exercise => exercise.title),
+    datasets: [
+      {
+        label: 'Number of Likes',
+        data: topExercises.map(exercise => exercise.likes),
+        backgroundColor: colors.slice(0, topExercises.length),
+        borderColor: borderColor.slice(0, topExercises.length),
+        borderWidth: 1,
+        datalabels: {
+          color: '#black',
+          formatter: (value, context) => {
+            return value; // Display the number of likes
+          },
+        },
+      },
+    ],
+  };
+
+  const topDislikedExercisesData = {
+    labels: topDislikedExercises.map(exercise => exercise.title),
+    datasets: [
+      {
+        label: 'Number of Dislikes',
+        data: topDislikedExercises.map(exercise => exercise.dislikes),
+        backgroundColor: colors.slice(0, topDislikedExercises.length),
+        borderColor: borderColor.slice(0, topDislikedExercises.length),
+        borderWidth: 1,
+        datalabels: {
+          color: '#black',
+          formatter: (value, context) => {
+            return value; // Display the number of dislikes
+          },
+        },
+      },
+    ],
+  };
+
   const options = {
     responsive: true,
     maintainAspectRatio: false,
+    plugins: {
+      datalabels: {
+        display: true,
+        color: 'black',
+        formatter: (value, context) => {
+          return value;
+        },
+        font: {
+          weight: 'bold',
+          size: 16,
+        },
+      },
+    },
     scales: {
       y: {
         beginAtZero: true,
         ticks: {
-          stepSize: 1, // Ensure integer step size for better visualization
+          stepSize: 1,
         },
       },
     },
@@ -146,6 +229,18 @@ const Dashboard = () => {
       <h2 className="flex justify-center text-2xl font-bold mb-4 text-white">Most Rated Posts</h2>
       <div className="bg-white shadow rounded-lg p-4 mb-8" style={{ height: '400px' }}>
         <Bar data={postRatingsData} options={options} />
+      </div>
+      <h2 className="flex justify-center text-2xl font-bold mb-4 text-white">Exercise Ratings</h2>
+      <div className="bg-white shadow rounded-lg p-4 mb-8" style={{ height: '400px' }}>
+        <Bar data={exerciseRatingsData} options={options} />
+      </div>
+      <h2 className="flex justify-center text-2xl font-bold mb-4 text-white">Top 5 Exercises by Likes</h2>
+      <div className="bg-white shadow rounded-lg p-4 mb-8" style={{ height: '400px' }}>
+        <Pie data={topExercisesData} options={options} plugins={[ChartDataLabels]} />
+      </div>
+      <h2 className="flex justify-center text-2xl font-bold mb-4 text-white">Top 5 Exercises by Dislikes</h2>
+      <div className="bg-white shadow rounded-lg p-4 mb-8" style={{ height: '400px' }}>
+        <Pie data={topDislikedExercisesData} options={options} plugins={[ChartDataLabels]} />
       </div>
     </div>
   );
