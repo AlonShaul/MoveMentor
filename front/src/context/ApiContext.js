f898e2c3bfb378ecb1c55408d63b05e8c25fe884
Alon Shaul <alon.shaul95@gmail.com>
2024-08-01 19:35:39 +0300

    Fix Bugs

--- front/src/context/ApiContext.js
+++ front/src/context/ApiContext.js
@@ -5,16 +5,7 @@ export const useApi = () => useContext(ApiContext);
 
 export const ApiProvider = ({ children }) => {
   let apiUrl = '';
-
-  try {
-    if (process.env.RENDER) {
       apiUrl = 'https://movementor-fyyf.onrender.com';
-    } else {
-      throw new Error('No URL');
-    }
-  } catch {
-    apiUrl = 'http://localhost:8800';
-  }
 
   return (
     <ApiContext.Provider value={apiUrl}>
