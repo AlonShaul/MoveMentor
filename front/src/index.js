33ba12c3236b0f20cc93111b45f5fe33b4201d78
Alon Shaul <alon.shaul95@gmail.com>
2024-08-01 22:12:54 +0300

    Fix Bug v3

--- front/src/index.js
+++ front/src/index.js
@@ -4,16 +4,19 @@ import App from './App';
 import { AuthProvider } from './context/authContext';
 import { CategoryProvider } from './context/CategoryContext';
 import { ApiProvider } from './context/ApiContext'; // Add ApiProvider here
+import { BrowserRouter } from 'react-router-dom'; // Add BrowserRouter
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
 root.render(
-  <ApiProvider> {/* Wrap with ApiProvider */}
-    <AuthProvider>
-      <CategoryProvider>
-        <App />
-      </CategoryProvider>
-    </AuthProvider>
-  </ApiProvider>
+  <BrowserRouter> {/* Wrap with BrowserRouter */}
+    <ApiProvider> {/* Wrap with ApiProvider */}
+      <AuthProvider>
+        <CategoryProvider>
+          <App />
+        </CategoryProvider>
+      </AuthProvider>
+    </ApiProvider>
+  </BrowserRouter>
 );
